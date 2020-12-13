5bd2cfea0b93bcde49016b540bab0f2e2ca95c4a
Valentina Peters <pvv1827@gmx.de>
2020-12-13 21:42:55 +0100

    add prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import gameEngine, { getRandom } from '../index.js';
+
+const CONDITION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  }
+
+  return 'yes';
+};
+
+const round = () => {
+  const question = getRandom(2, 100);
+
+  return [question, isPrime(question)];
+};
+
+export default () => {
+  gameEngine(CONDITION, round);
+};
