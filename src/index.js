5770dd36206637c8ac0127deefd562dc9358cf81
Valentina Peters <pvv1827@gmx.de>
2020-12-14 21:05:21 +0100

    fix index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import readlineSync from 'readline-sync';
-import greeting from './cli.js';
 
 const ROUNDS = 3;
+
+export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+
+  return name;
+};
 
 export const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
